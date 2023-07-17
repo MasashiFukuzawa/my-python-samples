416ae927465c29ea4237c21f8bc25eb4328dbbb6
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2023-07-17 22:08:22 +0900

    maturin build -i python3 --release

--- maturin/strcounter/src/lib.rs
+++ maturin/strcounter/src/lib.rs
@@ -1,14 +1,27 @@
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn count_chars(s: &str) -> (usize, usize, usize) {
+   let mut alphabet_count = 0;
+   let mut digit_count = 0;
+   let mut other_count = 0;
+
+    for c in s.chars() {
+        if c.is_ascii_alphabetic() {
+            alphabet_count += 1;
+        } else if c.is_ascii_digit() {
+            digit_count += 1;
+        } else {
+            other_count += 1;
+        }
+    }
+
+    (alphabet_count, digit_count, other_count)
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn strcounter(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(count_chars, m)?)?;
     Ok(())
 }
